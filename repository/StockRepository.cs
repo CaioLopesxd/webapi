fc8c7b1f13cf44664c27c8db291b9055c7115e6f
caiolopexd <r9506505@gmail.com>
2025-04-19 14:31:51 -0300

    feat: Adding pagination

--- repository/StockRepository.cs
+++ repository/StockRepository.cs
@@ -53,7 +53,9 @@ namespace webapi.repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
         public async Task<Stock?> GetByIdStock(uint id)
         {
